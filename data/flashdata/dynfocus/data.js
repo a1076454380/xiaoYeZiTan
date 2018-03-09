b05097b3edde83b44f873964a8f023d2b4f93430
董小礼 <1076454380@qq.com>
2018-03-09 15:08:49 +0800

    董礼

---
+++ data/flashdata/dynfocus/data.js
@@ -0,0 +1,13 @@
+imgUrl1="data/afficheimg/20170821aougsl.jpg";
+imgtext1="";
+imgLink1=escape("http://sc.xyzt.com/goods.php?id=432");
+imgUrl2="data/afficheimg/20170821buflnd.jpg";
+imgtext2="";
+imgLink2=escape("http://sc.xyzt.com/goods.php?id=439");
+imgUrl3="data/afficheimg/20170821gawxwx.jpg";
+imgtext3="";
+imgLink3=escape("http://sc.xyzt.com/goods.php?id=438");
+
+var pics=imgUrl1+"|"+imgUrl2+"|"+imgUrl3;
+var links=imgLink1+"|"+imgLink2+"|"+imgLink3;
+var texts=imgtext1+"|"+imgtext2+"|"+imgtext3;
